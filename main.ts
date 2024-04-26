f7f41037cc22f9f8d609dee9bad5e9fbc70ebeaf
mariope123 <167509355+mariope123@users.noreply.github.com>
2024-04-26 08:34:43 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+for (let index = 0; index < 14; index++) {
+	
+}
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
+})
